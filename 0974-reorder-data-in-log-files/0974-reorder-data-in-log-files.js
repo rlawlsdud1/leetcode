cc7f0e7b59275a6aaa682df95c91979c7d0ec69a
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-02-06 14:29:36 +0900

    Time: 9 ms (56.59%), Space: 54.7 MB (29.76%) - LeetHub

--- 0974-reorder-data-in-log-files/0974-reorder-data-in-log-files.js
+++ 0974-reorder-data-in-log-files/0974-reorder-data-in-log-files.js
@@ -4,27 +4,32 @@
  */
 var reorderLogFiles = function (logs) {
   const letterLogs = [];
-  const digisLogs = [];
+  const digitLogs = [];
 
-  logs.forEach((v) => {
-    const splitedLog = v.split(" ");
-    if (Number.isInteger(Number(splitedLog[1]))) {
-      digisLogs.push(v);
+  for (let i = 0; i < logs.length; i++) {
+    const log = logs[i].split(" ");
+    if (Number.isInteger(Number(log[1]))) {
+      digitLogs.push(log);
     } else {
-      letterLogs.push(v);
+      letterLogs.push(log);
     }
-  });
-  console.log(letterLogs);
+  }
 
-  const compare = (a, b) => {
-    const n = a
-      .slice(a.indexOf(" ") + 1)
-      .localeCompare(b.slice(b.indexOf(" ") + 1));
-    if (n) return n;
-    return a[0].localeCompare(b[0]);
+  const sortingFunc = (a, b) => {
+    const n = a.slice(1).join(",").localeCompare(b.slice(1).join(","));
+    // n이 -1이라면 a가 b보다 먼저 온다
+    if (n === 0) return a[0].localeCompare(b[0]);
+    return n;
   };
 
-  letterLogs.sort(compare);
+  letterLogs.sort(sortingFunc);
+  const answer = [];
 
-  return letterLogs.concat(digisLogs);
+  letterLogs.forEach((v) => {
+    answer.push(v.join(" "));
+  });
+  digitLogs.forEach((v) => {
+    answer.push(v.join(" "));
+  });
+  return answer;
 };
