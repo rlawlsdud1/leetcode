168431c4647dd94e8cb2d0b295c22606074f139f
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-02-06 11:58:20 +0900

    Time: 84 ms (27.71%), Space: 54.8 MB (69.15%) - LeetHub

--- 0322-coin-change/0322-coin-change.js
+++ 0322-coin-change/0322-coin-change.js
@@ -3,19 +3,20 @@
  * @param {number} amount
  * @return {number}
  */
-var coinChange = function (coins, amount) {
-  const dp = Array.from({ length: amount + 1 }).fill(Infinity);
-  dp[0] = 0;
-
-  for (let i = 1; i <= amount; i++) {
-    let minCount = Infinity;
-    for (let coin of coins) {
-      if (i - coin >= 0) {
-        minCount = Math.min(minCount, dp[i - coin] + 1);
-      }
+var coinChange = function(coins, amount) {
+    const dp = Array.from({length:amount+1}).fill(Infinity)
+    dp[0] = 0
+    for(let i=1; i<=amount; i++){
+        for(let j=0; j<coins.length; j++){
+            if(coins[j] <= i){
+                dp[i] = Math.min(dp[i], dp[i-coins[j]] + 1) 
+            }
+        }
     }
-    dp[i] = minCount;
-  }
+    if(dp[amount] === Infinity){
+        return -1
+    }else{
+        return dp[amount]
+    }
 
-  return dp[amount] === Infinity ? -1 : dp[amount];
 };
