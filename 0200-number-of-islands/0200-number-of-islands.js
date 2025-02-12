2a47f7f0944a3017e41e95adbdd3b3bae45042d6
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-02-12 18:45:21 +0900

    Time: 83 ms (40.6%), Space: 57.5 MB (45.02%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -0,0 +1,39 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function (grid) {
+  const directions = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1],
+  ];
+
+  function DFS(x, y) {
+    for (let i = 0; i < 4; i++) {
+      const [nx, ny] = [x + directions[i][0], y + directions[i][1]];
+      if (
+        nx >= 0 &&
+        ny >= 0 &&
+        nx < grid.length &&
+        ny < grid[0].length &&
+        grid[nx][ny] === "1"
+      ) {
+        grid[nx][ny] = "0";
+        DFS(nx, ny);
+      }
+    }
+  }
+
+  let answer = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === "1") {
+        DFS(i, j);
+        answer++;
+      }
+    }
+  }
+  return answer;
+};
