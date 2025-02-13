0263f0e7d6534067c4668afbe589d489c2290a3c
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-02-13 15:59:22 +0900

    Time: 5 ms (7.36%), Space: 49.7 MB (24.42%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,12 +3,19 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function (nums) {
+  let swapped;
+
   for (let j = 0; j < nums.length; j++) {
-    for (let i = 0; i < nums.length - 1; i++) {
+    swapped = false;
+
+    for (let i = 0; i < nums.length - 1 - j; i++) {
       if (nums[i] > nums[i + 1]) {
         [nums[i], nums[i + 1]] = [nums[i + 1], nums[i]];
+        swapped = true;
       }
     }
+
+    if (!swapped) break;
   }
 
   return nums;
