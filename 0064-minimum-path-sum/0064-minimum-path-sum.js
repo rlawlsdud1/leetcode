3193ab0a4288b4802b3e85cc586062e42df937c2
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-02-17 00:15:47 +0900

    Time: 11 ms (27.35%), Space: 52.1 MB (39.61%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.js
+++ 0064-minimum-path-sum/0064-minimum-path-sum.js
@@ -3,19 +3,22 @@
  * @return {number}
  */
 var minPathSum = function (grid) {
-  const [n, m] = [grid.length, grid[0].length];
+  const n = grid.length;
+  const m = grid[0].length;
+
   const dp = Array.from({ length: n }, () => Array.from({ length: m }).fill(0));
   dp[0][0] = grid[0][0];
 
+  for (let i = 1; i < n; i++) {
+    dp[i][0] += dp[i - 1][0] + grid[i][0];
+  }
   for (let i = 1; i < m; i++) {
-    dp[0][i] = grid[0][i] + dp[0][i - 1];
+    dp[0][i] += dp[0][i - 1] + grid[0][i];
   }
-  for (let j = 1; j < n; j++) {
-    dp[j][0] = grid[j][0] + dp[j - 1][0];
-  }
+
   for (let i = 1; i < n; i++) {
     for (let j = 1; j < m; j++) {
-      dp[i][j] = Math.min(dp[i - 1][j] + grid[i][j], dp[i][j - 1] + grid[i][j]);
+      dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
     }
   }
 
