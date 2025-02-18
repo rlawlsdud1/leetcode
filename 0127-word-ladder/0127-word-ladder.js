5378925147fe18e1b70a0228547bc9aa2663a6a6
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-02-19 01:06:22 +0900

    Time: 1282 ms (6.79%), Space: 60.8 MB (27.35%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {number}
+ */
+var ladderLength = function (beginWord, endWord, wordList) {
+  const visited = Array.from({ length: wordList.length }).fill(false);
+
+  const queue = [];
+  queue.push([beginWord, 1]);
+
+  while (queue.length) {
+    const [target, count] = queue.shift();
+    // console.log(target, count);
+
+    if (target === endWord) return count;
+
+    for (let i = 0; i < wordList.length; i++) {
+      if (validateWord(target, wordList[i]) && !visited[i]) {
+        queue.push([wordList[i], count + 1]);
+        visited[i] = true;
+      }
+    }
+  }
+
+  return 0;
+
+  function validateWord(str1, str2) {
+    let count = 0;
+    for (let i = 0; i < str1.length; i++) {
+      if (str1[i] !== str2[i]) {
+        count++;
+      }
+    }
+
+    if (count > 1) {
+      return false;
+    }
+    return true;
+  }
+};
