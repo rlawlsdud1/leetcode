8f960a0cb8390f5ff969b729cc6e5933d26518bc
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-02-19 14:45:45 +0900

    Time: 3 ms (67.75%), Space: 55.8 MB (5.18%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.js
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -3,19 +3,16 @@
  * @return {number}
  */
 var maxProduct = function (nums) {
-  // 특정 index를 tail로 하는 곱의 최솟값
-  let minProduct = nums[0];
-
-  // 특정 index를 tail로 하는 곱의 최댓값
   let maxProduct = nums[0];
+  let minProduct = nums[0];
+  let answer = nums[0];
 
-  let answer = Math.max(maxProduct, minProduct);
   for (let i = 1; i < nums.length; i++) {
-    const maxTemp = maxProduct;
-    const minTemp = minProduct;
-    maxProduct = Math.max(maxTemp * nums[i], nums[i], minTemp * nums[i]);
-    minProduct = Math.min(minTemp * nums[i], nums[i], maxTemp * nums[i]);
+    let tempMax = maxProduct;
+    let tempMin = minProduct;
 
+    maxProduct = Math.max(tempMax * nums[i], nums[i], tempMin * nums[i]);
+    minProduct = Math.min(tempMax * nums[i], nums[i], tempMin * nums[i]);
     answer = Math.max(maxProduct, answer);
   }
 
