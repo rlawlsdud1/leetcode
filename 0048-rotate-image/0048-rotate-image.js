2cd402c410ce9abbf4943e27616fdae9c9ba569d
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-02-20 13:46:12 +0900

    Time: 0 ms (100%), Space: 53.8 MB (5.64%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < i; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+
+  return matrix.map((v) => v.reverse());
+};
