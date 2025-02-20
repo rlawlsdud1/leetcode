dc7e11ebb18889c071bcfd4a1144043b9fade37c
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-02-20 23:48:04 +0900

    Time: 2 ms (76.47%), Space: 64.6 MB (5.14%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,13 +3,12 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-  let current = prices[0];
+  let cur = prices[0];
   let profit = 0;
 
-  // 최대한 가격이 낮을 때 사야, 최대한의 이득을 볼 가능성이 생긴다
   for (let i = 1; i < prices.length; i++) {
-    if (current > prices[i]) current = prices[i];
-    profit = Math.max(profit, prices[i] - current);
+    if (prices[i] < cur) cur = prices[i];
+    profit = Math.max(profit, prices[i] - cur);
   }
 
   return profit;
