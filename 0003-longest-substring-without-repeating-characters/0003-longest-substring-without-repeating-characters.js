539cb22313631f30f68ee912fd55bb23b407f65e
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-02-21 17:12:09 +0900

    Time: 17 ms (38.17%), Space: 60.5 MB (5.03%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,17 +3,19 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-  let [left, right] = [0, 0];
-  let answer = 0;
+  let left = 0,
+    right = 0,
+    answer = 0;
+
   const hashTable = {};
 
   while (right < s.length) {
-    if (hashTable[s[right]]) {
+    while (hashTable[s[right]]) {
       delete hashTable[s[left++]];
-    } else {
-      answer = Math.max(answer, right - left + 1);
-      hashTable[s[right++]] = 1;
     }
+    hashTable[s[right]] = 1;
+    answer = Math.max(answer, right - left + 1);
+    right++;
   }
 
   return answer;
