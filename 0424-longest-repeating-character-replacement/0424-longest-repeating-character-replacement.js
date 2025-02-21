0d8002d4c5fc37fdcfb9f1832b97bb6ed90f1257
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-02-22 01:21:56 +0900

    Time: 23 ms (54.11%), Space: 55.2 MB (26.26%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -5,22 +5,23 @@
  */
 var characterReplacement = function (s, k) {
   let left = 0,
-    freqCount = 0,
+    freqCnt = 0,
     answer = 0;
+
   const hashTable = {};
 
   for (let right = 0; right < s.length; right++) {
     if (hashTable[s[right]]) hashTable[s[right]] += 1;
     else hashTable[s[right]] = 1;
 
-    freqCount = Math.max(freqCount, hashTable[s[right]]);
+    freqCnt = Math.max(freqCnt, hashTable[s[right]]);
 
-    // 윈도우크기가 최빈값+k 보다 크다면 윈도우 크기를 줄여야한다
-    while (right - left + 1 > freqCount + k) {
+    while (right - left + 1 > freqCnt + k) {
       hashTable[s[left++]] -= 1;
     }
 
     answer = Math.max(answer, right - left + 1);
   }
+
   return answer;
 };
