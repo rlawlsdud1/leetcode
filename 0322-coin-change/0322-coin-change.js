71a26d5c993ed3349c16f0df5464ab93a074c9ba
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-02-25 16:44:31 +0900

    Time: 85 ms (26.2%), Space: 60.5 MB (9.29%) - LeetHub

--- 0322-coin-change/0322-coin-change.js
+++ 0322-coin-change/0322-coin-change.js
@@ -4,6 +4,9 @@
  * @return {number}
  */
 var coinChange = function (coins, amount) {
+  // dp[i] 는 i원을 만들 수 있는 최소 개수 조합
+  // dp[i] = Math.min(dp[i-1], dp[i-2], dp[i-2]) + 1
+
   const dp = Array.from({ length: amount + 1 }).fill(Infinity);
   dp[0] = 0;
 
@@ -14,8 +17,6 @@ var coinChange = function (coins, amount) {
       }
     }
   }
-  if (dp[amount] !== Infinity) {
-    return dp[amount];
-  }
-  return -1;
+  if (dp.at(-1) === Infinity) return -1;
+  return dp.at(-1);
 };
