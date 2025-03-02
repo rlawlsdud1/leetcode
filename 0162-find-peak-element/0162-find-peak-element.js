e8a0777085fd0ad1af82ff4c9a2afd31a6712265
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-03-02 14:33:16 +0900

    Time: 0 ms (100%), Space: 53.8 MB (8.14%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -9,7 +9,7 @@ var findPeakElement = function (nums) {
   while (left <= right) {
     const mid = Math.floor((left + right) / 2);
 
-    if (nums[mid] <= nums[mid + 1]) {
+    if (nums[mid] < nums[mid + 1]) {
       left = mid + 1;
     } else {
       right = mid - 1;
