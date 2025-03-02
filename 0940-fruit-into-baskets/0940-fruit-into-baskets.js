8c7006112c128622538477c8c19991b5a255b4eb
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-03-02 15:58:25 +0900

    Time: 264 ms (18.9%), Space: 71.8 MB (5.5%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} fruits
+ * @return {number}
+ */
+var totalFruit = function (fruits) {
+  let left = 0,
+    answer = 0;
+  const hashTable = {};
+
+  for (let right = 0; right < fruits.length; right++) {
+    if (hashTable[fruits[right]]) hashTable[fruits[right]] += 1;
+    else hashTable[fruits[right]] = 1;
+
+    while (Object.keys(hashTable).length > 2) {
+      if (hashTable[fruits[left]]) hashTable[fruits[left]] -= 1;
+      if (!hashTable[fruits[left]]) delete hashTable[fruits[left]];
+      left++;
+    }
+
+    answer = Math.max(answer, right - left + 1);
+  }
+  return answer;
+};
