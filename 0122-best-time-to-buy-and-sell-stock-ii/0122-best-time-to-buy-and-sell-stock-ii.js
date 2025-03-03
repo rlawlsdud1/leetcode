b310abc17baa8a45b38b1e237dc6565e31a944ba
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-03-03 15:28:00 +0900

    Time: 2 ms (34.44%), Space: 55.7 MB (5.11%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -1,11 +1,17 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
 var maxProfit = function (prices) {
+  let curr = Infinity;
   let profit = 0;
 
-  for (let i = 0; i < prices.length - 1; i++) {
-    if (prices[i] < prices[i + 1]) {
-      profit += prices[i + 1] - prices[i];
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < curr) curr = prices[i];
+    else {
+      profit += prices[i] - curr;
+      curr = prices[i];
     }
   }
-
   return profit;
 };
