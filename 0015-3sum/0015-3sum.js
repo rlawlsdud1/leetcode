55ff40d01a1d688357f89bd5a48a96f815063c3a
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-03-03 20:04:13 +0900

    Time: 1355 ms (5.01%), Space: 73.4 MB (5.05%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -1,32 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
 var threeSum = function (nums) {
-  nums.sort((a, b) => a - b); // 배열 정렬
-  const answer = [];
+  // 순서 상관 없다고 했으므로, 정렬하고 시작하자
+  nums.sort((a, b) => a - b);
+  const answer = new Set();
 
-  for (let fixedPointer = 0; fixedPointer < nums.length - 2; fixedPointer++) {
-    // 중복된 fixedPointer 건너뛰기
-    if (fixedPointer > 0 && nums[fixedPointer] === nums[fixedPointer - 1]) continue;
-
-    let left = fixedPointer + 1;
-    let right = nums.length - 1;
+  for (let pointer = 0; pointer < nums.length - 2; pointer++) {
+    let left = pointer + 1,
+      right = nums.length - 1;
 
     while (left < right) {
-      const sum = nums[fixedPointer] + nums[left] + nums[right];
-
+      const sum = nums[pointer] + nums[left] + nums[right];
       if (sum === 0) {
-        answer.push([nums[fixedPointer], nums[left], nums[right]]);
-        // 중복된 left와 right 건너뛰기
-        while (left < right && nums[left] === nums[left + 1]) left++;
-        while (left < right && nums[right] === nums[right - 1]) right--;
+        answer.add([nums[pointer], nums[left], nums[right]].join(","));
         left++;
+      } else if (sum > 0) {
         right--;
-      } else if (sum < 0) {
-        left++; // 합이 작으면 left를 오른쪽으로 이동
       } else {
-        right--; // 합이 크면 right를 왼쪽으로 이동
+        left++;
       }
     }
   }
-  return answer;
-};
 
-console.log(threeSum([3, 0, -2, -1, 1, 2]));
+  return [...answer].map((v) => v.split(",").map(Number));
+};
