a3416f3272e8f6aaea74d0f23f76d214640f20a2
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-03-03 20:07:39 +0900

    Time: 0 ms (100%), Space: 53.8 MB (5.68%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -3,12 +3,13 @@
  * @return {number}
  */
 var climbStairs = function (n) {
-  const dp = Array.from({ length: n + 2 }).fill(0);
-  dp[1] = 1;
+  const dp = Array.from({ length: n }).fill(0);
+  dp[0] = 1;
+  dp[1] = 2;
 
-  for (let i = 2; i < n + 2; i++) {
+  for (let i = 2; i < n; i++) {
     dp[i] = dp[i - 1] + dp[i - 2];
   }
 
-  return dp[n + 1];
+  return dp[n - 1];
 };
